dcce25a336b5e4b6dca31286c0285c390702a8d8
JosePabloOcampo1212 <jp.ocampo1000@gmail.com>
2025-04-22 15:46:04 -0600

    fix

--- plugins/action/security_advisories_results_advisories_id_network_devices_network_device_id_info.py
+++ plugins/action/security_advisories_results_advisories_id_network_devices_network_device_id_info.py
@@ -2,8 +2,7 @@
 # -- coding: utf-8 --
 from ansible.plugins.action import ActionBase
 from ansible_collections.cisco.catalystcenter.plugins.action \
-    .security_advisories_results_advisories_id_network_devices \
-    .network_device_id_v1_info import ActionModule
+    .security_advisories_results_advisories_id_network_devices_network_device_id_v1_info import ActionModule
 
 
 class ActionModule2(ActionBase):
